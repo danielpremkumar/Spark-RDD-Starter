d1130c22f4c09a025c985cba4b2179b2e7c087ed
nerdylegend <31123377+nerdylegend@users.noreply.github.com>
2019-07-19 10:03:06 +0530

    Top 10 Word Count without boring words

---
+++ src/main/java/com/virtualpairprogrammers/TopWordCount.java
@@ -0,0 +1,35 @@
+package com.virtualpairprogrammers;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TopWordCount {
+    public static void main(String[] args) {
+        // Only for Windows
+        System.setProperty("hadoop.home.dir", "C:/hadoop");
+
+        // Below Program gives Top 10 highest word count occurances from the big data (boring words filtered)
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+            JavaRDD<String> initialRDD = sc.textFile("src/main/resources/subtitles/input.txt");
+            List<Tuple2<Long, String>> sorted = initialRDD.map(s -> s.replaceAll("[^a-zA-Z\\s]", "").toLowerCase())
+                    .filter(s-> s.trim().length() > 0)
+                    .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+                    .filter(s -> s.trim().length() > 0 && Util.isNotBoring(s))
+                    .mapToPair(word -> new Tuple2<String, Long>(word, 1L))
+                    .reduceByKey((val1, val2) -> val1 + val2)
+                    .mapToPair(pair -> new Tuple2<Long, String>(pair._2, pair._1))
+                    .sortByKey(false)
+                    .take(10);
+            sorted.forEach(System.out::println);
+        }
+    }
+}
